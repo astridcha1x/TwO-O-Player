76311375faf4c4411ad4fe17ec90f139d2abc6f7
Astrid Aguilar <mixaguilar@yahoo.com>
2021-06-25 17:03:33 +0000

    Creates classes defined in their own rb files

---
+++ game.rb
@@ -0,0 +1,19 @@
+class Game
+
+  def initialize(game_start)
+    @game_start = game_start
+  end
+
+  def initialize(game_end)
+    @game_end = game_end
+  end
+
+  def initialize(current_player)
+    @current_player = current_player
+  end
+
+  def initialize(winning_player)
+    @winning_player = winning_player
+  end
+
+end
---
+++ player.rb
@@ -0,0 +1,11 @@
+class Player
+
+  def initialize(player_one)
+    @player_one = player_one
+  end
+
+  def initialize(player_two)
+    @player_two = player_two
+  end
+
+end
